743dace5aa13a9453f2c4322e50fee81cb4e1379
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-02 22:42:25 +0200

    move_validation.py: further development

--- move_validation.py
+++ move_validation.py
@@ -15,16 +15,28 @@ def move_validation(a: list, b: list, board: list) -> dict:
 
     # check if there is a piece at the selected field
     piece = board[a[0]][a[1]]
-    print(piece)
+    print(f'piece: {piece}')
     if piece == '+':
         return {'validation': False, 'figure': None, 'message': ReturnCodes.EMPTY_FIELD.value}
     else:
         intended_move = [b[0]-a[0], b[1]-a[1]]
         length = sqrt(intended_move[0]**2 + intended_move[1]**2)
         intended_move_norm = [1/length * intended_move[0], 1/length * intended_move[1]]
-        print(intended_move)
-        print(intended_move_norm)
-        # directions_of_move = pieces_config
+
+        # get directions of move
+        for figure in pieces_config.keys():
+            print(figure)
+            for colour in pieces_config[figure]:
+                print(colour)
+                if pieces_config[figure][colour]['sign'] == piece:
+                    directions_of_move = pieces_config[figure][colour]['directions_of_movement']
+                    print(directions_of_move, intended_move_norm)
+
+        if intended_move_norm not in directions_of_move:
+            return {'validation': False, 'figure': None, 'message': ReturnCodes.INVALID_MOVE.value}
+
+
+
 
 
 if __name__ == '__main__':
