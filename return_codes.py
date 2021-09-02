37699bd69c2358f8c222d2766b1557dcb388b46c
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-02 22:41:25 +0200

    return_codes.py: adds new codes

--- return_codes.py
+++ return_codes.py
@@ -8,4 +8,7 @@ class ReturnCodes(Enum):
     BEAT_FIGURE = 'Valid move, beat opponents figure'
     WRONG_INPUT_FORMAT = "Wrong input format. The first character must be a letter from A-H," \
                          " the second one a number from 1-8"
+    EMPTY_FIELD = 'Empty field selected.'
+    SAME_COORDINATES = 'Input coordinates are identical'
+    ENTRY_NOT_FOUND = 'Entry not found'
 
