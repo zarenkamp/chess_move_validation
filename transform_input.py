00cb77124aeeba4a79026078d1526001f8fb1e60
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-04 00:01:38 +0200

    transform_input.py: clean up

--- transform_input.py
+++ transform_input.py
@@ -6,7 +6,7 @@ def transform_into_coordinates(letter_plus_code: str):
     # left corner to simplify iteration over nested list
     mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
                '1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
-    letter_plus_code = letter_plus_code.upper()
+
     if len(letter_plus_code) == 2\
             and letter_plus_code[0].isalpha()\
             and letter_plus_code[0] in mapping.keys()\
