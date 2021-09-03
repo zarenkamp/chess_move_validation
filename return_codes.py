3dd02a3a54f81c93f009ee74bc46fa19178d37a0
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-03 22:19:43 +0200

    return_codes.py: adds new codes

--- return_codes.py
+++ return_codes.py
@@ -6,7 +6,7 @@ class ReturnCodes(Enum):
     INVALID_MOVE = 'Invalid move, please read the chess instructions again'
     MOVE_BLOCKED_OWN = f'Move blocked through own figure'
     BEAT_FIGURE = 'Valid move, beat opponents figure'
-    WRONG_INPUT_FORMAT = "Wrong input format. The first character must be a letter from A-H," \
+    WRONG_INPUT_FORMAT = "Wrong input. The first character must be a letter from A-H," \
                          " the second one a number from 1-8"
     EMPTY_FIELD = 'Empty field selected.'
     SAME_COORDINATES = 'Input coordinates are identical'
