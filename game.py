092b285a0fa0a8d3240265f318d13a69ade92b45
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-03 22:18:46 +0200

    game.py: adds new methods

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import random
 from transform_input import transform_into_coordinates
 from init_board import init_board
 from draw_board import draw_board
+from check_input import check_input, check_field
 
 
 class ChessGame:
@@ -17,18 +18,38 @@ class ChessGame:
             print(ReturnCodes.CONFIG_ERROR.value, e)
 
         self.occupied_fields = {}
-
         self.board, self.occupied_fields = init_board(self.pieces_config, rand_order)
-
-        print(self.occupied_fields)
-
+        print('Board ready...')
         draw_board(self.board)
 
+    def get_intended_move(self):
 
+        while True:
+            piece_to_move = input('Which piece to move?').upper()
+            user_input = check_input(piece_to_move)
+            if user_input['result']:
+                check = check_field(piece_to_move, self.occupied_fields)
+                if check['result']:
+                    print(f'{check["colour"].capitalize()} {check["piece"].lower()} selected')
+                    break
+                else:
+                    print(check['message'])
+            else:
+                print(user_input['message'])
 
+        while True:
+            target_field = input('Where to move?').upper()
+            res = check_input(target_field)
+            if res['result']:
+                break
+            else:
+                print(res['message'])
+
+        print(f'{check["colour"].capitalize()} {check["piece"].lower()} {piece_to_move} --> {target_field}')
 
 
 
 
 if __name__ == '__main__':
     game = ChessGame(rand_order=True)
+    game.get_intended_move()
