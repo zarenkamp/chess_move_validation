b3d71ee8e5860dc8c1bd4e1436ba38fd0438523b
Jonas Fahrenkamp <jonas.fahrenkamp@allgeier-engineering.com>
2021-09-04 16:30:02 +0200

    main.py: mainloop

--- main.py
+++ main.py
@@ -4,14 +4,44 @@ main game
 from src.game import ChessGame
 
 if __name__ == '__main__':
+    try:
+        while True:
+            while True:
+                rand_order = input('Place pieces randomly? y/n')
+                if rand_order not in ['n', 'y']:
+                    print('Wrong input!')
+                    continue
+                else:
+                    if rand_order == 'y':
+                        rand_order = True
+                    else:
+                        rand_order = False
+                    break
 
-    while True:
-        try:
-            game = ChessGame(rand_order=True)
-            game.get_intended_move()
-            game.validate_move()
+            game = ChessGame(rand_order)
 
-            break
-        except Exception as e:
-            print(e)
-            break
+            while True:
+
+                game.get_intended_move()
+                game.validate_move()
+                while True:
+                    new_try = input('Another try -> 1, new board -> 2, quit game -> 3?')
+
+                    if new_try not in ['1', '2', '3']:
+                        print('Wrong input!')
+                        continue
+                    else:
+                        break
+
+                if new_try == '1':
+                    continue
+                else:
+                    break
+            if new_try == '2':
+                continue
+            else:
+                break
+
+    except Exception as e:
+        print(e)
+
